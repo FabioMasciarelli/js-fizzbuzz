6a1427c31d159f4199e8ae13a4c5a94e1f35b283
Fabio Masciarelli <fabiomasciarelliwd@gmail.com>
2024-03-28 12:00:40 +0100

    change: scope print

--- js/script.js
+++ js/script.js
@@ -8,23 +8,21 @@ for(i = 1; i <= 100; i++) {
     let text = i;
     let type =`number`;
     const square = `square`;
-    if(i % 3 === 0) {
-        console.log(i);
-        text = `Fizz`;
-        type = `fizz`;
-        console.log(text);
-    }
 
-    if (i % 5 === 0){
+    
+    if (i % 5 === 0 && i % 3 === 0) {
+        text =`FizzBuzz`;
+        type = `fizzbuzz`;
+        console.log(text);
+    } else if (i % 5 === 0) {
         console.log(i);
         text = `Buzz`;
         type = `buzz`;
         console.log(text);
-    }
-
-    if (i % 5 === 0 && i % 3 === 0) {
-        text =`FizzBuzz`;
-        type = `fizzbuzz`;
+    } else if (i % 3 === 0) {
+        console.log(i);
+        text = `Fizz`;
+        type = `fizz`;
         console.log(text);
     }
 
